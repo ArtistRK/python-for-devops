16369b85d5de4f2ee2558a07a63fe08f303cf041
Rishab Kanoujia <108139937+ArtistRK@users.noreply.github.com>
2025-01-14 16:44:39 +0000

    Day-04 Prac

---
+++ Day-04/calculator.py
@@ -0,0 +1,17 @@
+
+
+def addittion(numb1, numb2):
+    add = numb1 + numb2
+    return add
+
+def sub(numb1, numb2):
+    s = numb1 - numb2
+    return s
+
+def mul(numb1, numb2):
+    m = numb1 * numb2
+    return m
+
+addittion(5, 6)
+sub()
+mul()
